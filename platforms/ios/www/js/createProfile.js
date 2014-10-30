3c54a5b8806acaa1343a27e7dcd8e86aced4cfe6
Dre Mcdonald <andre.mcdonald@luxuryretreats.com>
2014-10-29 23:29:45 -0400

    update

--- platforms/ios/www/js/createProfile.js
+++ platforms/ios/www/js/createProfile.js
@@ -1,3 +1,17 @@
 /**
  * Created by andremcdonald on 14-10-29.
  */
+
+document.addEventListener('deviceready', deviceReady, false);
+
+function deviceReady() {
+    initHandlers();
+}
+
+
+function initHandlers(){
+    $("#submitBtn").on("touchend", function(){
+        $("#list").show();
+    });
+}
+
