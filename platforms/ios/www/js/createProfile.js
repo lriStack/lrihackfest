c891646728f5be729c4535812083c33cb029c308
Dre Mcdonald <andre.mcdonald@luxuryretreats.com>
2014-10-30 10:37:12 -0400

    update

--- platforms/ios/www/js/createProfile.js
+++ platforms/ios/www/js/createProfile.js
@@ -9,9 +9,69 @@ function deviceReady() {
 }
 
 
-function initHandlers(){
-    $("#submitBtn").on("touchend", function(){
-        $("#list").show();
+function initHandlers() {
+
+
+    var listFlag = 0;
+
+
+    function yourInterests() {
+        var source = $("#yourInterestsList").html();
+        var template = Handlebars.compile(source);
+
+        var data = {
+            yourLikes: [
+                {
+                    CategoryType: "Camera"
+                },
+                {
+                    CategoryType: "Smart Phone"
+
+                },
+                {
+                    CategoryType: "Tablet"
+                },
+                {
+                    CategoryType: "Computer"
+                }
+            ]
+        };
+
+        if (listFlag === 0) {
+            $('body').append(template(data));
+        }
+    }
+
+
+    var values = [];
+    $('.categoryGrp').click(function (el) {
+        //console.log($(this).attr('checked'));
+        //$('#result').append($(this).attr('value') + ',');
+        values.push($(this).attr('value'));
+        var str = values.join(", ");
+//        alert(str);
+        localStorage.catList = str;
     });
+
+
+    $("#submitBtn").on("click", function () {
+        $("#pickList").show();
+        yourInterests();
+        listFlag = 1;
+
+    });
+
+
 }
+
+function setchoices() {
+
+
+
+}
+
+
+
+
+
 
